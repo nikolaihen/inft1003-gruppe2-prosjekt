f362fa1fb6bcb9d15054806f1d9ac3767cfd1d18
Nikolai Henriksen <nikolhe@stud.ntnu.no>
2020-11-15 20:49:33 +0100

    Added instructions page

--- src/website/menu/menu.js
+++ src/website/menu/menu.js
@@ -1,9 +1,17 @@
 $(document).ready(() => {
-    $('#startGameButton').click(onGameStart);
+  $('#startGameButton').click(onGameStart);
+  $('#instructionsButton').click(onInstructionsClicked);
 
-    function onGameStart() {
-      $('#game-view').show();
-      $('.menu').hide();
-      $('body').css("background-image", "url('./assets/background_gif_darker.gif')");
-    }
-  });
+  function onGameStart() {
+    $('#game-view').show();
+    $('.menu').hide();
+    $('body').css("background-image", "url('./assets/background_gif_darker.gif')");
+  }
+
+  function onInstructionsClicked() {
+    $(document).add('<div><div/>')
+    $('#instructions-view').show();
+    $('.menu').hide();
+    $('body').css("background-image", "url('./assets/background_gif_darker.gif')");
+  }
+});
